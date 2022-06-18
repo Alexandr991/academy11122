55d6a864d177d2fd8298acd933136f00f1084ca7
Milevskiy <ajleksandr991@gmail.com>
2022-06-18 17:54:53 +0300

    test

--- src/main/java/by/academy/lesson3/Task4.java
+++ src/main/java/by/academy/lesson3/Task4.java
@@ -1,4 +1,29 @@
 package by.academy.lesson3;
 
 public class Task4 {
+    public static void main(String[] args) {
+        int s = 234_370_000;
+        int sec;
+        int m;
+        int min;
+        int h;
+        int day;
+        int week;
+        sec = s % 60;
+        m = (s - sec) / 60;
+        min = m % 60;
+        h = (m - min) / 60;
+        System.out.println(h + " часов " + min + " минут "
+                + sec + " секунд");
+        day = (h / 24);
+        h %= 24;
+        System.out.println(day + " Дней " + h + " часов " + min + " минут "
+                + sec + " секунд");
+        week = day / 7;
+        day %= 7;
+        System.out.println(week + " Недель " + day + " Дней " + h + " часов " + min + " минут "
+                + sec + " секунд");
+
+
+    }
 }
---
+++ src/main/java/by/academy/lesson3/Task5.java
@@ -0,0 +1,26 @@
+package by.academy.lesson3;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int average;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number a :");
+        a = scan.nextInt();
+        System.out.println("Enter number b :");
+        b = scan.nextInt();
+        if (a > b) {
+            System.out.println("Max number:" + a + "\n" + "Min number:" + b);
+        } else if (a == b) {
+            System.out.println("The numbers are equal." + "\n" + "Max value :" + a);
+        } else {
+            System.out.println("Max number:" + b + "\n" + "Min number:" + a);
+        }
+        average = (a+b)/2;
+        System.out.println("Average value:" + average);
+    }
+}
+
