af11ebf22f6797cbee6332f9f436df3e73df45ad
Milevskiy <ajleksandr991@gmail.com>
2022-06-22 23:47:59 +0300

    test

---
+++ src/main/java/by/academy/lesson4/Task6.java
@@ -0,0 +1,21 @@
+package by.academy.lesson4;
+
+import java.util.Scanner;
+
+public class Task6 {
+    public static void main(String[] arg) {
+        double deposit;
+        int year;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter you sum of money:");
+        deposit = scan.nextDouble();
+        System.out.println("Enter number of years:");
+        year = scan.nextInt();
+        for (int i = 1; i <= year; i++) {
+            deposit = deposit + (deposit * 0.03);
+
+        }
+        System.out.println("You deposit after " + year + " years will be " + deposit);
+
+    }
+}
